f718b39bae58e5a4dd8241f373fb75065d8eceef
Naikar <Sathis.Naikar@otis.com>
2025-01-07 02:29:49 +0530

    latest commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const db=require('./db');
 require('dotenv').config();
-
+//command to check git
 const bodyParser=require('body-parser');
 
 
